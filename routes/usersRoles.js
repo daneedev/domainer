dd658f70c87b230eac777496b6fb153338000274
daneeskripter <imdanee@seznam.cz>
2023-10-27 20:40:08 +0200

    deleteRole & editRole routes added

--- routes/usersRoles.js
+++ routes/usersRoles.js
@@ -4,6 +4,8 @@ const { checkAuth, checkAdmin } = require("../handlers/checkAuth")
 const User = require("../models/User")
 const router2 = express.Router()
 const Role = require("../models/Role")
+const router3 = express.Router()
+const router4 = express.Router()
 
 router.post("/", checkAuth, checkAdmin, async function (req, res) {
     const findUser = await User.findOne({username: req.body.username})
@@ -36,5 +38,54 @@ router2.post("/", checkAuth, checkAdmin, async function (req, res) {
     }
 })
 
+router3.post("/", checkAuth, checkAdmin, async function (req, res) {
+    const role = req.body.role
+    const findRole = await Role.findOne({name: role})
+    if (!findRole) {
+        req.flash("adminerror", "That role doesn't exist!")
+        res.redirect("/admin")
+    } else {
+        const findUsers = await User.find({role: role})
+        if (findUsers.length > 0) {
+            req.flash("adminerror", "There are still users with that role!")
+            res.redirect("/admin")
+        } else {
+            await Role.deleteOne({name: role})
+            res.redirect("/admin")
+        }
+    }
+})
+
+router4.get("/", checkAuth, checkAdmin, async function (req, res) {
+    const role = req.query.role
+    const findRole = await Role.findOne({name: role})
+    if (!findRole) {
+        req.flash("adminerror", "That role doesn't exist!")
+        res.redirect("/admin")
+    } else {
+        res.render(__dirname + "/../views/editrole.ejs", {role: role})
+    }
+})
+
+router4.post("/", checkAuth, checkAdmin, async function (req, res) {
+    const role = req.body.role
+    const maxSubdomains = req.body.maxSubdomains
+    const newname = req.body.name
+    const findRole = await Role.findOne({name: role})
+    const findUsers = await User.findOne({role: role})
+    if (!findRole) {
+        req.flash("adminerror", "That role doesn't exist!")
+        res.redirect("/admin")
+    } else if (findUsers) {
+        req.flash("adminerror", "There are still users with that role!")
+        res.redirect("/admin")
+    } else {
+        await Role.updateOne({name: role}, {name: newname, maxSubdomains: maxSubdomains})
+        res.redirect("/admin")
+    }
+})
+
 module.exports.deleteUser = router
 module.exports.changeRole = router2
+module.exports.deleteRole = router3
+module.exports.editRole = router4
--- server.js
+++ server.js
@@ -89,6 +89,7 @@ app.use("/toreview", require("./routes/admin").review)
 
 app.use("/deleteuser", require("./routes/usersRoles").deleteUser)
 app.use("/changerole", require("./routes/usersRoles").changeRole)
-
+app.use("/deleterole", require("./routes/usersRoles").deleteRole)
+app.use("/editrole", require("./routes/usersRoles").editRole)
 
 app.listen(3000, () => console.log('Server running on port 3000'));
