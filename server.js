e31dee16c4c65b964a50379c93a069698a52e172
daneeskripter <imdanee@seznam.cz>
2024-03-06 15:31:57 +0100

    mongodb connection replace by sqlite one

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const express = require('express');
 const app = express();
 const passport = require('passport');
 const path = require("path")
-const mongoose = require("mongoose")
 const flash = require("connect-flash")
 const session = require("cookie-session")
 const initializePassport = require("./handlers/passport")
@@ -20,6 +19,11 @@ const limiter = RateLimit({
   max: 100
 });
 app.use(limiter);
+
+// SYNC DB
+const sequelize = require('./database');
+
+sequelize.sync().then(() => console.log("Database is ready!"))
 
 if (process.env.SETUPED == "yes") {
 initializePassport(passport)
@@ -41,26 +45,19 @@ app.use(session({
 
 async function checkDefaultRole() {
 const Role = require("./models/Role")
-const findDefaultRole = await Role.findOne({name: "default"})
+const findDefaultRole = await Role.findOne({where:{name: "default"}})
 if (!findDefaultRole) {
-const DefaultRole = new Role({
+Role.create({
   name: "default",
   maxSubdomains: 5,
   default: true
 })
-DefaultRole.save()
 }
 }
 checkDefaultRole()
 }
 app.use(express.urlencoded({ extended: false }))
 app.use(flash())
-
-mongoose.connect(process.env.MONGO_SRV, {}).then(() => {
-    console.log("Connected to the database!")
-}).catch((err) => {
-    console.log("Failed connect to the database!")
-})
 
 const cf = require("cloudflare")({
     token: process.env.CLOUDFLARE_API_TOKEN
